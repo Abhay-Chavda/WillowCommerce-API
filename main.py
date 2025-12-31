cad40d01c0a1f0d23e8c028d47d54e1daad42832
Abhay-Chavda <118757273+Abhay-Chavda@users.noreply.github.com>
2025-12-31 13:47:23 +0530

    commit files given

---
+++ main.py
@@ -0,0 +1,25 @@
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from openai import get_openai_client
+
+myEndpoint = "https://amar-0558-resource.services.ai.azure.com/api/projects/amar-0558"
+
+project_client = AIProjectClient(
+    endpoint=myEndpoint,
+    credential=DefaultAzureCredential(),
+)
+
+myAgent = "WillowCommerce-Agent"
+# Get an existing agent
+agent = project_client.agents.get(agent_name=myAgent)
+print(f"Retrieved agent: {agent.name}")
+
+openai_client = project_client.get_openai_client()
+
+# Reference the agent to get a response
+response = openai_client.responses.create(
+    input=[{"role": "user", "content": "Tell me what you can help with."}],
+    extra_body={"agent": {"name": agent.name, "type": "agent_reference"}},
+)
+
+print(f"Response output: {response.output_text}")
